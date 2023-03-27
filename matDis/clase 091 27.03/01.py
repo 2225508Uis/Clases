d96b2a40da17144beed2173d9c17aba19874f3db
2225508Uis <128964193+2225508Uis@users.noreply.github.com>
2023-03-27 15:00:05 +0000

    save

--- matDis/clase 091 27.03/01.py
+++ matDis/clase 091 27.03/01.py
@@ -1,1 +1,10 @@
+while True:
+  print("a) añadir numero a la lista: ")
+  print("b) añadir numero a la lista: ")
+  print("c) añadir numero a la lista: ")
+  print("d) añadir numero a la lista: ")
+  print("e) añadir numero a la lista: ")
+  print("f) añadir numero a la lista: ")
+  print("g) añadir numero a la lista: ")
+  print("h) añadir numero a la lista: ")
 
