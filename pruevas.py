33a8ed271c7d3995728415cd28f08c0d6a1fdf91
2225508Uis <128964193+2225508Uis@users.noreply.github.com>
2023-03-28 23:03:44 +0000

    save

--- pruevas.py
+++ pruevas.py
@@ -1,25 +1,21 @@
 import matplotlib.pyplot as plt
 
-# x-coordinates of left sides of bars
-left = [1, 2, 3, 4, 5]
+# Datos de prueba
+x = [1, 2, 3, 4, 5]
+y1 = [2, 4, 6, 8, 10]
+y2 = [3, 6, 9, 12, 15]
 
-# heights of bars
-height = [10, 24, 36, 40, 5]
+# Creación de la gráfica
+plt.plot(x, y1, label='Línea 1')
+plt.plot(x, y2, label='Línea 2')
 
-# labels for bars
-tick_label = ['one', 'two', 'three', 'four', 'five']
+# Título y etiquetas del eje
+plt.title('Gráfica con dos líneas')
+plt.xlabel('Eje X')
+plt.ylabel('Eje Y')
 
-# plotting a bar chart
-plt.bar(left, height, tick_label = tick_label,  width = 0.8)
+# Leyenda
+plt.legend()
 
-# naming the x-axis
-plt.xlabel('x - axis')
-
-# naming the y-axis
-plt.ylabel('y - axis')
-
-# plot title
-plt.title('My bar chart!')
-
-# function to show the plot
+# Mostrar gráfica
 plt.show()
