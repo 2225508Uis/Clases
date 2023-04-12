d55405bd05de748961252d89595b8a54bdba9663
2225508Uis <128964193+2225508Uis@users.noreply.github.com>
2023-04-12 00:32:29 +0000

    save taller de permutaciones

--- pruevas.py
+++ pruevas.py
@@ -1,24 +1,15 @@
 """Module providingFunction printing python version."""
 
 import matplotlib.pyplot as plt
-
+plt.matplotlib.use("Agg")
+import os
 
 # Datos de prueba
 x = [1, 2, 3, 4, 5]
 y1 = [2, 4, 6, 8, 10]
 y2 = [3, 6, 9, 12, 15]
 
-# Creación de la gráfica
-plt.plot(x, y1, label='Línea 1')
-plt.plot(x, y2, label='Línea 2')
-
-# Título y etiquetas del eje
-plt.title('Gráfica con dos líneas')
-plt.xlabel('Eje X')
-plt.ylabel('Eje Y')
-
-# Leyenda
-plt.legend()
-
-# Mostrar gráfica
-plt.show()
+fig,ax = plt.subplots()
+ax.plot(x,y1)
+fig.savefig("grafica.png")
+os.system("cat grafica.png")
